150adf5fde268ebf3fbd8d782c4aae4626ca74e9
Mike Tubis <mike.tubis@gmail.com>
2017-09-12 01:35:25 +0200

    add tableView

---
+++ ynd/ListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ListCell.swift
+//  ynd
+//
+//  Created by Michał Tubis on 12.09.2017.
+//  Copyright © 2017 Mike Tubis. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    @IBOutlet weak var listImage: UIImageView!
+    @IBOutlet weak var listName: UILabel!    
+
+}
---
+++ ynd/MainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainViewController.swift
+//  ynd
+//
+//  Created by Michał Tubis on 12.09.2017.
+//  Copyright © 2017 Mike Tubis. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - View lifecyclet5
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    // MARK: - TableView delegate
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.register(UINib(nibName: "ListCell", bundle: nil), forCellReuseIdentifier: "ListCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath as IndexPath) as? ListCell
+        
+        return cell!
+        }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
